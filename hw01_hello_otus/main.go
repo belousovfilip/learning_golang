ae5ba4ebbf5720142d6bd89aa030d0999c3c9882
philip <belousovfilip@gmail.com>
2022-06-03 20:13:34 +0300

    TEST

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+)
+
 func main() {
-	// Place your code here.
+	var words = map[string]int{}
+	group := []struct {
+		name string
+		count int
+	}{}
+	reg := regexp.MustCompile(`[ ]+`)
+	res := reg.Split("sad sd as d d d asd sa ds dasd sad sadfgvfdgfsdg fg sdf dsf sdf sf sd fsd f", -1)
+	for _, v := range res {
+		fmt.Println(words["sd"])
+		words[v]++
+	}
+	fmt.Println(words.)
 }
