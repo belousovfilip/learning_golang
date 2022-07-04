2ab06d592cf6b7bd1a30a43c2f516b09bd217f44
philip <belousovfilip@gmail.com>
2022-07-04 03:14:16 +0300

    add functions for list item

--- hw04_lru_cache/listItem.go
+++ hw04_lru_cache/listItem.go
@@ -5,3 +5,19 @@ type ListItem struct {
 	Next  *ListItem
 	Prev  *ListItem
 }
+
+func (l *ListItem) isFirst() bool {
+	return l.Prev == nil
+}
+
+func (l *ListItem) isLast() bool {
+	return l.Next == nil
+}
+
+func (l *ListItem) isInside() bool {
+	return l.isLast() == false && l.isFirst() == false
+}
+
+func NewListItem(i interface{}) *ListItem {
+	return &ListItem{i, nil, nil}
+}
