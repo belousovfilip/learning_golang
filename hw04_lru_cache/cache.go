87b424dfbba55736df93b7872712e3514214dd7c
philip <belousovfilip@gmail.com>
2022-07-04 05:16:31 +0300

    cache interface implementation

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,16 +9,53 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
 }
 
-type cacheItem struct {
+func (l *lruCache) Set(key Key, value interface{}) bool {
+	element, exists := l.items[key]
+	if exists == true {
+		l.queue.MoveToFront(element)
+		element.Value.(*CacheItem).value = value
+		return true
+	}
+	if l.queue.Len() == l.capacity {
+		listItem := l.queue.Back()
+		l.queue.Remove(listItem)
+		delete(l.items, listItem.Value.(*CacheItem).key)
+	}
+	item := NewCacheItem(key, value)
+	element = l.queue.PushFront(item)
+	l.items[item.key] = element
+	return false
+}
+
+func (l *lruCache) Get(key Key) (interface{}, bool) {
+	element, exists := l.items[key]
+	if exists == false {
+		return nil, false
+	}
+	l.queue.MoveToFront(element)
+	return element.Value.(*CacheItem).value, true
+}
+
+func (l *lruCache) Clear() {
+	l.queue = NewList()
+	l.items = make(map[Key]*ListItem, l.capacity)
+}
+
+type CacheItem struct {
 	key   Key
 	value interface{}
+}
+
+func NewCacheItem(key Key, value interface{}) *CacheItem {
+	return &CacheItem{
+		key:   key,
+		value: value,
+	}
 }
 
 func NewCache(capacity int) Cache {
